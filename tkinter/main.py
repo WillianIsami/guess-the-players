f4917a78bd5bc03bbdf96b5fb0cc5b9480a96bf6
WillianIMS <willianisami1229@gmail.com>
2023-08-02 00:03:16 -0300

    Addition of configurations for the game to work

--- tkinter/main.py
+++ tkinter/main.py
@@ -1,7 +1,4 @@
 import random
-import os
-import json
-import sys
 import tkinter as tk
 import sqlite3
 import csv 
@@ -9,6 +6,8 @@ from tkinter import messagebox
 
 class GameGUI:
     def __init__(self, root):
+        self.db_connection = sqlite3.connect("database.db")
+        self.create_table()
         self.root = root
         self.root.title("Jogo da Advinhação")
         self.root.geometry("400x300")
@@ -16,17 +15,12 @@ class GameGUI:
         self.user_label.pack()
         self.user_entry = tk.Entry(root)
         self.user_entry.pack()
-        self.db_connection = sqlite3.connect("database.db")
-        self.create_table()
         self.start_button = tk.Button(root, text="Iniciar Jogo", command=self.start_game)
         self.start_button.pack()
         self.pontuation = 0
         self.counter = 0
-        self.player = None
-        self.player_list = []
-        self.player_index = 0
-        self.random_player = self.get_random_player("players_21.csv")
-        # TODO: Another file to store the database settings.
+        self.flag = 0
+        self.show_tips(True)
 
     def start_game(self):
         self.user = self.user_entry.get().strip()
@@ -40,65 +34,75 @@ class GameGUI:
         self.user_label.pack_forget()
         self.user_entry.pack_forget()
         self.start_button.pack_forget()
-        self.verify_player()
+        self.create_widgets()
 
-    def get_random_player(self, file_path):
-        with open(file_path, newline='', encoding='utf-8') as csvfile:
-            reader = csv.reader(csvfile)
-            players = list(reader)
-        random_player = random.choice(players)
-        return random_player
-
-    def show_tips(self):
-        print("Jogo da Advinhação")
-        print("Acerte o nome do jogador através das dicas de posição, time e nacionalidade do jogador")
-        print("Você tem 5 chances para acertar o jogador!")
+    def show_tips(self, new_player=False, show_tip=False):
+        if new_player:
+            with open("players_21.csv", newline='', encoding='utf-8') as csvfile:
+                reader = csv.reader(csvfile)
+                players = list(reader)
+            self.random_player = random.choice(players)
         _, short_name, long_name, age, birth_day, height, weight, nationality, team, league_name, overall, potential, positions, preferred_foot = self.random_player
         tips = {
-            "age":age,
-            "birth_day":birth_day,
-            "height":height,
-            "weight":weight,
-            "nationality":nationality,
-            "team":team,
-            "league_name":league_name,
-            "overall":overall,
-            "potential":potential,
-            "positions":positions,
-            "preferred_foot":preferred_foot
+            "age:":age,
+            "birth_day:":birth_day,
+            "height:":height,
+            "weight:":weight,
+            "nationality:":nationality,
+            "team:":team,
+            "league_name:":league_name,
+            "overall:":overall,
+            "potential:":potential,
+            "positions:":positions,
+            "preferred_foot:":preferred_foot
         }
         tip_one, tip_two = random.choice(list(tips.items()))
-        return (f"{tip_one} - {tip_two}")
-    
-    def widget(self):
+        if show_tip:
+            tip_label = tk.Label(self.root, text=f"{tip_one} - {tip_two}")
+            tip_label.pack()
+        return short_name, long_name
+
+    def create_widgets(self):
         self.guess_label = tk.Label(self.root, text="Quem é o jogador?")
         self.guess_label.pack()
-        self.guess_entry = tk.Entry(root)
+        self.guess_entry = tk.Entry(self.root)
         self.guess_entry.pack()
-        self.guess_button = tk.Button(root, text="Enviar", command=self.verify_player)
+        self.guess_button = tk.Button(self.root, text="Enviar", command=self.verify_guess)
         self.guess_button.pack()
-
-    def verify_player(self, guess_entry):
-        guess = guess_entry.get().strip()
-        short_name, long_name = self.random_player[1:3]
+    
+    def verify_guess(self):
+        guess = self.guess_entry.get()
+        short_name, long_name = self.show_tips()
+        print(f"{guess.lower()} - {short_name.lower()} - {long_name.lower()}\n")
         if (guess.lower() == short_name.lower()) or (guess.lower() == long_name.lower()):
             messagebox.showinfo("Parabéns", "Você acertou o jogador!")
-            self.pontuation += 5
+            self.pontuation += 10
             self.counter += 1
-            self.player_label.pack_forget()
-            self.guess_entry.pack_forget()
+            self.guess_label.pack_forget()
             self.guess_button.pack_forget()
-            return
+            self.wrong_answer_label.pack_forget()
+            self.wrong_answer_entry.pack_forget()
+            self.wrong_answer_tip.pack_forget()
+            messagebox.showinfo("Game Over", f"Sua pontuação final: {self.pontuation} pontos")
+            self.root.after(5000, self.close_app)
         else:
-            if self.pontuation:
+            self.counter += 1
+            if self.counter > 5:
                 messagebox.showinfo("Fim de Jogo", f"O jogador era {short_name}. Acabou as suas tentativas. {self.user}, você ficou com {self.pontuation} pontos.")
-                self.verify_player.pack_forget()
-                sys.exit()
-            messagebox.showinfo("Resposta Errada", "Você errou! Quer receber +1 dica? (-1 ponto).")
-            tk.Button(self.root, text="Receber +1 dica", command=self.show_tips)
-            self.pontuation -= 1
-            self.verify_player()
-    
+                self.wrong_answer_label.pack_forget()
+                self.wrong_answer_tip.pack_forget()
+                self.root.after(5000, self.close_app)
+            self.flag += 1
+            if self.flag == 1:
+                self.wrong_answer_label = tk.Label(self.root, text="Você errou! Quer receber +1 dica? (-1 ponto).")
+                self.wrong_answer_label.pack()
+                self.wrong_answer_tip = tk.Button(self.root, text="Receber +1 dica", command=lambda: self.show_tips
+                (False, True))
+                self.wrong_answer_tip.pack()
+
+    def close_app(self):
+        self.root.destroy()
+
     def create_table(self):
         with self.db_connection:
             self.db_connection.execute('''
