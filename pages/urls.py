ac2e37c990ab693d24be3bc5fd450ccedfa5dfd2
WillianIMS <willianisami1229@gmail.com>
2023-08-02 00:05:53 -0300

    Organization of files

--- pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import IndexView
-from django.urls import re_path
-
-urlpatterns = [
-    # path('endereço/', minhaview.as_view(), name='nome-da-url'),
-    path('', IndexView.as_view(), name='home'),
-]
